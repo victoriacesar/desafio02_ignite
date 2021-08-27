dc80d2efe29da343a658d39fc3b936c8a7e0a19b
Victoria Cesar <victoriacesaras@gmail.com>
2021-08-27 15:15:55 -0300

    code complete

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface SideBarProps {
+  genres: Array<{
+    id: number;
+    title: string;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  }>;
+  clickFunc(id: number): void;
+  selectedGenreId: number;
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.clickFunc(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
